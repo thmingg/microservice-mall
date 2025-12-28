7892e22b1f56f603e9a7381648e5bfd259fc96c2
s1021025 <>
2025-12-29 02:43:59 +0800

    chore: init project structure

---
+++ mall-coupon/src/test/java/com/thmingg/microservicemall/mallcoupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thmingg.microservicemall.mallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
